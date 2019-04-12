b0db0e315dcb31414be8f32e3f4b5249403daa5c
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-12 12:18:22 -0400

    POST and GET games endpoints created in server file

--- api/server.js
+++ api/server.js
@@ -10,12 +10,17 @@ server.get('/', async (req, res) => {
   res.status(200).json({ message: "Welcome to Lambda Testing Sprint Challenge 2019" });
 });
 
-
-
-server.get('/api/games', async (req, res) => {
+server.post('/api/games', async (req, res) => {
   const savedGames = await games.getAll();
 
   res.status(200).json(savedGames);
+});
+
+server.get('/api/games', async (req, res) => {
+  const gameInfo = req.body;
+  const newGame = await games.insert(gameInfo);
+
+  res.status(200).json(newGame);
 });
 
 module.exports = server;
