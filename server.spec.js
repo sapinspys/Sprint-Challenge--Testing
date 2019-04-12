7d511152790d3a676b5a4e972f3be245461138d0
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-12 11:16:53 -0400

    Created three tests for GET /

--- server.spec.js
+++ server.spec.js
@@ -1,3 +1,22 @@
 const request = require('supertest');
 const server = require('./server.js');
 
+describe('server.js', () => {
+  describe('GET /', () => {
+    it('should return 200 OK status code', () => {
+
+    })
+
+    it('should return JSON format response', () => {
+      
+    })
+
+    it('should return a welcome message', () => {
+      
+    })
+  })
+
+  describe('GET /', () => {
+
+  })
+})
