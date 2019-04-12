607ae796143470e523b984208e5342d863d87a73
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-12 13:07:30 -0400

    Updated POST endpoint

--- api/server.js
+++ api/server.js
@@ -1,23 +1,30 @@
-const express = require('express');
+const express = require("express");
 
-const games = require('../games/games-model.js');
+const games = require("../games/games-model.js");
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/', async (req, res) => {
-  res.status(200).json({ message: "Welcome to Testing Sprint Challenge - Lambda 2019" });
+server.get("/", async (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Welcome to Testing Sprint Challenge - Lambda 2019" });
 });
 
-server.post('/api/games', async (req, res) => {
+server.post("/api/games", async (req, res) => {
   const gameInfo = req.body;
-  const newGame = await games.insert(gameInfo);
 
-  res.status(201).json(newGame);
+  if (gameInfo.title && gameInfo.genre) {
+    const newGame = await games.insert(gameInfo);
+
+    res.status(201).json(newGame);
+  } else {
+    res.status(422).json({ error: "Game title and genre are required fields" });
+  }
 });
 
-server.get('/api/games', async (req, res) => {
+server.get("/api/games", async (req, res) => {
   const savedGames = await games.getAll();
 
   res.status(200).json(savedGames);
