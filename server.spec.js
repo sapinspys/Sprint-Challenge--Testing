6701308a6febd5da4cfca2e1e9e257088f2fe064
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-12 11:49:57 -0400

    Populated three more tests

--- server.spec.js
+++ server.spec.js
@@ -1,88 +1,84 @@
-const request = require('supertest');
-const server = require('./server.js');
+const request = require("supertest");
+const server = require("./server.js");
 
-describe('server.js', () => {
-  describe('GET /', () => {
-    it('should return 200 OK status code', () => {
+describe("server.js", () => {
+  describe("GET /", () => {
+    it("should return 200 OK status code", () => {
       return request(server)
         .get("/")
-        .expect(200)
-    })
+        .expect(200);
+    });
 
-    it('should return JSON format response', () => {
-      
-    })
+    it("should return JSON format response", () => {
+      return request(server)
+        .get("/")
+        .expect("Content-Type", /json/);
+    });
 
-    it('should return a welcome message', () => {
-      
-    })
-  })
+    it("should return a welcome message", () => {});
+  });
 
-  describe('POST /api/games', () => {
-    const pacman =   {
-      title: 'Pacman', // required
-      genre: 'Arcade', // required
+  describe("POST /api/games", () => {
+    const pacman = {
+      title: "Pacman", // required
+      genre: "Arcade", // required
       releaseYear: 1980 // not required
-    }
-    
-    it('should return 201 OK status code with all data', () => {
+    };
+
+    it("should return 201 OK status code with all data", () => {
       return request(server)
-      .post("/api/games")
-      .send(pacman)
-      .expect(201)
+        .post("/api/games")
+        .send(pacman)
+        .expect(201);
     });
 
-    it('should return 201 OK status even with missing year', () => {
+    it("should return 201 OK status even with missing year", () => {
       return request(server)
-      .post("/api/games")
-      .send({...pacman, releaseYear: null})
-      .expect(201)
+        .post("/api/games")
+        .send({ ...pacman, releaseYear: null })
+        .expect(201);
     });
 
-    it('should return 422 status code if title is blank', () => {
+    it("should return 422 status code if title is blank", () => {
       return request(server)
-      .post("/api/games")
-      .send({...pacman, title: ""})
-      .expect(422)
+        .post("/api/games")
+        .send({ ...pacman, title: "" })
+        .expect(422);
     });
 
-    it('should return 422 status code if genre is blank', () => {
+    it("should return 422 status code if genre is blank", () => {
       return request(server)
-      .post("/api/games")
-      .send({...pacman, genre: ""})
-      .expect(422)
+        .post("/api/games")
+        .send({ ...pacman, genre: "" })
+        .expect(422);
     });
 
-    it('should return error message if required fields are blank', () => {
-      
-    });
-
-    it('should return JSON format response', () => {
-      
-    })
-
-    it('should return created game', () => {
-      
-    })
-  })
-
-  describe('GET /api/games', () => {
-    it('should return 200 OK status code', () => {
+    it("should return JSON format response", () => {
       return request(server)
-      .get("/api/games")
-      .expect(200)
+        .get("/api/games")
+        .expect("Content-Type", /json/);
     });
 
-    it('should return JSON format response', () => {
-      
-    })
+    it("should return error message if required fields are blank", () => {});
 
-    it('should return an empty array if there are no games to return', () => {
-      
-    })
+    it("should return created game", () => {});
+  });
 
-    it('should return an array of all games', () => {
-      
-    })
-  })
-})
+  describe("GET /api/games", () => {
+    it("should return 200 OK status code", () => {
+      return request(server)
+        .get("/api/games")
+        .expect(200);
+    });
+
+    it("should return JSON format response", () => {
+      return request(server)
+        .get("/api/games")
+        .expect("Content-Type", /json/);
+    });
+
+    it("should return an empty array if there are no games to return", () => {});
+
+    it("should return an array of all games", () => {});
+  });
+});
